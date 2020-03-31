1886f877c91e0006189751f53fd721abf09a6a8e
mak-aravind-lab <aravind.murugaiyan@gmail.com>
2020-03-31 13:18:33 +0530

    each problem will be addressed by a individual project. This to enhance the solutions from brute force to best solutions.

---
+++ algoExpert/app/src/main/java/org/mak/algoexpert/AppHelloWorld.java
@@ -0,0 +1,7 @@
+package org.mak.algoexpert;
+
+public class AppHelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello!! World from App Main");
+    }
+}
---
+++ algoExpert/easy-question-two-number-sum/src/main/java/org/mak/algoexpert/TwoNumberSum.java
@@ -0,0 +1,7 @@
+package org.mak.algoexpert;
+
+public class TwoNumberSum {
+    public static void main(String[] args){
+        System.out.println("Hello!! World from TwoNumberSum");
+    }
+}
